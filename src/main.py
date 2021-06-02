844eb12d23e94ceb3830e68b230d14e9c6818502
Augustin Harter <chibatz@gmail.com>
2021-06-02 12:41:59 +0200

    added automatic model save and load and methods for inference

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ if __name__ == '__main__':
     # SETUP ARGUMENT PARSER
     argpar = argparse.ArgumentParser()
     argpar.add_argument("--epochs", default=30)
+    argpar.add_argument("-noload", action="store_true")
+    argpar.add_argument("-train", action="store_true")
     args = argpar.parse_args()  
 
     # SETUP PARSER
@@ -18,14 +20,38 @@ if __name__ == '__main__':
     print(f"vocab size: {vocab_size}")
     model = word2Vec.CustomWord2Vec(vocab_size=vocab_size)
     model.configure_optimizer()
-
-    # SETUP TRAIN DATA
+    loaded_model_flag = False
+    if not args.noload:
+        loaded_model_flag = model.load_model()
+    
+    # SETUP DATA
     contexts, centers = AS.generate_contexts(action_sequences)
     np_contexts = AS.actions_to_tokenized_np_arrays(contexts, action_to_id)
     np_centers = AS.actions_to_tokenized_np_arrays(centers, action_to_id)
     data_loader = model.data_loader_from_numpy(np_centers.squeeze(), np_contexts)
 
     # TRAINING
-    print(f"Start of Training for {args.epochs} epochs")
-    model.train(data_loader, epochs=args.epochs)
-    model.plot_logs(["loss"])
+    if not loaded_model_flag or args.train:
+        print(f"Start of Training for {args.epochs} epochs")
+        model.train(data_loader, epochs=args.epochs)
+        model.plot_logs(["loss"])
+
+    # TESTING
+    #TODO: way to get action from idxs
+    # maybe juts this:?
+    idx_to_action = lambda idx: list(action_to_id.keys())[list(action_to_id.values()).index(idx)]
+    print("action for idx 12", idx_to_action(12))
+
+    #TODO then maybe something like this:
+    vec12 = model.idx_to_center_vec(12)
+    vec15 = model.idx_to_center_vec(15)
+    delta_vec = vec12 - vec15
+
+    print(model.get_most_similar_idxs(vec=delta_vec))
+    print(model.get_most_similar_idxs(idx=12))
+    print(model.get_most_similar_idxs(idx=15))
+
+    #TODO clustering of embedding vectors?
+    # cluster(model.centers)
+    # cluster(model.contexts)
+
--- src/models/word2Vec.py
+++ src/models/word2Vec.py
@@ -5,19 +5,22 @@ import torch.utils.data as data
 import numpy as np
 from matplotlib import pyplot as plt
 import tqdm
+import os
 
 class CustomWord2Vec(nn.Module):
     def __init__(self, vocab_size: int = 30000, dims: int = 64,
-                 path: str = "default-model/") -> None:
+                 name: str = "default-word2vec") -> None:
         super().__init__()
         self.vocab_size = vocab_size
         self.dims = dims
-        self.path = path
+        self.path = "res/models/"+name+"/"
         self.centers = T.randn(vocab_size, dims, requires_grad=True)
         self.contexts = T.randn(vocab_size, dims, requires_grad=True)
         self.log = {"loss": []}
         self.neg_freq_fac = 1
         self.device = "cuda" if T.cuda.is_available() else "cpu"
+        self.save_every = 5
+        self.plot_every = 1
 
     def training_step(self, batch):
         # BATCH
@@ -56,7 +59,7 @@ class CustomWord2Vec(nn.Module):
     def train(self, train_loader: data.DataLoader, epochs=10, print_every=20) -> None:
         self.centers.to(self.device)
         self.contexts.to(self.device)
-        for epoch in range(epochs):
+        for epoch in range(1, epochs+1):
             t = tqdm.tqdm(train_loader)
             for b_idx, batch in enumerate(t):
                 for b in batch:
@@ -68,22 +71,66 @@ class CustomWord2Vec(nn.Module):
                     msg = f"epoch {epoch} loss {round(loss, 3)}"
                     t.set_description(msg)
 
+            if not epoch%self.plot_every:
+                self.plot_logs(["loss"])
+            if not epoch%self.save_every:
+                self.save_model()
+        
+
     def data_loader_from_numpy(self, centers, contexts, batch_size=32,
                                shuffle=True) -> data.DataLoader:
         dataset = data.TensorDataset(T.from_numpy(centers), T.from_numpy(contexts))
         return data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
-    def plot_logs(self, keys):
+    def plot_logs(self, keys, show=False):
+        os.makedirs(self.path, exist_ok=True)
         for key in keys:
             if self.log[key]:
                 plt.clf()
                 values = self.get_moving_avg(self.log[key], n=30)
-                plt.plot(values)
-                plt.show()
+                plt.plot(values, label=key)
+                if show:
+                    plt.show()
+                plt.savefig(self.path+"plots.png")
 
     def get_moving_avg(self, x, n=10):
         cumsum = np.cumsum(x)
         return (cumsum[n:] - cumsum[:-n]) / n
+
+    def idx_to_center_vec(self, idx):
+        return self.centers[idx]
+
+    def idx_to_context_vec(self, idx):
+        return self.contexts[idx]
+
+    def get_most_similar_idxs(self, idx=None, vec=None, centers=False, top_n=10):
+        assert idx is not None or vec is not None
+        contained = int(vec is None)
+        vectors = self.centers if centers else self.contexts
+        if contained:
+            vec = vectors[idx]
+
+        norm = T.norm(vec)
+        all_norms = T.norm(vectors, dim=1)
+        sims = vec.matmul(vectors.T)/(norm*all_norms)
+        top = T.topk(sims, top_n+contained)[1][contained:10+contained]
+        return top.tolist()
+
+    def save_model(self):
+        os.makedirs(self.path,  exist_ok=True)
+        print(f"saving model to {self.path}")
+        T.save(self.centers, self.path+"centers.pt")
+        T.save(self.contexts, self.path+"contexts.pt")
+
+    def load_model(self):
+        if os.path.exists(self.path):
+            print(f"loading model from {self.path}")
+            self.centers = T.load(self.path+"centers.pt")
+            self.contexts = T.load(self.path+"contexts.pt")
+            return True
+        else:
+            print(f"Couldn't find save files in path {self.path} -> nothing loaded!")
+            return False
 
 
 if __name__ == "__main__":
